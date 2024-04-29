eb79d4798c27a4abff558b61fa9305aeedb10604
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-04-29 09:26:50 +0200

    generate xml feed only if some product change

--- src/api/feed-creator/controllers/feed-creator.js
+++ src/api/feed-creator/controllers/feed-creator.js
@@ -4,20 +4,23 @@ const fs = require('node:fs');
 
 module.exports = {
   async create(ctx) {
-    const products = await strapi.entityService.findMany('api::product.product',  {populate: '*'});
-    try {
-      ctx.body = 'ok';
-      var writeStream = fs.createWriteStream("public/feed_zbozi.xml");
-      writeStream.write('<?xml version="1.0" encoding="utf-8"?>');
-      writeStream.write('<SHOP xmlns="http://www.zbozi.cz/ns/offer/1.0">');
-      products.forEach(p => {
-        writeStream.write(strapi.service('api::feed-creator.feed-creator').generateShopItem(p));
-      })
-      writeStream.write('\n</SHOP>\n');
-      writeStream.end();
-    } catch (err) {
-      ctx.body = err;
+    if (ctx.request.body.model === "product") {
+      const products = await strapi.entityService.findMany('api::product.product', {populate: '*'});
+      try {
+        ctx.body = 'ok';
+        var writeStream = fs.createWriteStream("public/feed_zbozi.xml");
+        writeStream.write('<?xml version="1.0" encoding="utf-8"?>');
+        writeStream.write('<SHOP xmlns="http://www.zbozi.cz/ns/offer/1.0">');
+        products.forEach(p => {
+          writeStream.write(strapi.service('api::feed-creator.feed-creator').generateShopItem(p));
+        })
+        writeStream.write('\n</SHOP>\n');
+        writeStream.end();
+      } catch (err) {
+        ctx.body = err;
+      }
     }
+    return ctx.send({ message: "xml file is created" });
   }
 };
 
